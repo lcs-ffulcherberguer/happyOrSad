cf6129d0e28510e48376fa834a50cf6e7d41ec13
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-18 11:20:46 -0500

    Initial Commit

---
+++ happyOrSad/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  happyOrSad
+//
+//  Created by Fulcherberguer, Fernanda on 2019-11-18.
+//  Copyright © 2019 Fulcherberguer, Fernanda. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
