54bdc213e721160f0de32d044509bd8a105ea45e
Fernanda Fulcherberguer <ffulcherberguer@lcs.on.ca>
2019-11-20 11:54:28 -0500

    Did the guard let to tell the user the error

--- happyOrSad/ViewController.swift
+++ happyOrSad/ViewController.swift
@@ -28,6 +28,27 @@ class ViewController: UIViewController {
    //Analyse the text provided
     @IBAction func analyzeText(_ sender: Any) {
         
+        //1. INPUT
+        //Clear out the output label from the last time the Analyze button was pressed
+        outputResult.text = ""
+        
+        //Guard against no input
+        guard let phraseInput = phraseInputField.text, phraseInput.count > 0 else {
+            outputResult.text = "Please enter a phrase to analyze."
+            return
+            
+        }
+        
+        //Make sure the input is not too long
+        if phraseInput.count > 255 {
+            outputResult.text = "Please enter a phrase with 255 characters or less."
+            return
+        }
+       
     }
+    
+    
+    
+    
 }
 
